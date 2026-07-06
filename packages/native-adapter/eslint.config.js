b84dd0d6e1de58c427d3e7ea0759dd3b07302560
Joshua Farayola <farayolajoshua@gmail.com>
2026-07-06 14:42:46 +0100

    feat: add initial configuration files for native-adapter
    Includes ESLint, TypeScript, Vitest, and build configurations.

---
+++ packages/native-adapter/eslint.config.js
@@ -0,0 +1,7 @@
+import { config as baseConfig } from "@repo/eslint-config/base";
+
+/**
+ * A custom ESLint configuration for this package.
+ *
+ * @type {import("eslint").Linter.Config[]} */
+export const config = [...baseConfig];
